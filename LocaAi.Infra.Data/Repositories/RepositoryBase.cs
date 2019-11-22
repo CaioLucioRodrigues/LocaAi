ecc129d879345d78fe4a6169f9c9c623cbb1ae38
Caio Rodrigues <caio.rodrigues@ecalc.com.br>
2019-11-22 00:18:57 -0300

    Incluindo repositório base para entidade Usuário

---
+++ LocaAi.Infra.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,47 @@
+﻿using LocaAi.Domain.Interfaces.Repositories;
+using LocaAi.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LocaAi.Infra.Data.Repositories
+{
+    public class RepositoryBase<TEntity> : IDisposable, IRepositoryBase<TEntity> where TEntity : class
+    {
+        protected LocaAiContext Db = new LocaAiContext();
+
+        public void Add(TEntity obj)
+        {
+            Db.Set<TEntity>().Add(obj);
+            Db.SaveChanges();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return Db.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return Db.Set<TEntity>().ToList();
+        }
+
+        public void Update(TEntity obj)
+        {
+            Db.Entry(obj).State = EntityState.Modified;
+            Db.SaveChanges();
+        }
+
+        public void Remove(TEntity obj)
+        {
+            Db.Set<TEntity>().Remove(obj);
+            Db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LocaAi.Infra.Data/Repositories/UsuarioRepository.cs
@@ -0,0 +1,9 @@
+﻿using LocaAi.Domain.Entities;
+using LocaAi.Domain.Interfaces.Repositories;
+
+namespace LocaAi.Infra.Data.Repositories
+{
+    public class UsuarioRepository : RepositoryBase<Usuario>, IUsuarioRepository
+    {
+    }
+}
